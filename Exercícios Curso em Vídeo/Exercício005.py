17a8ef63eab9ca341eba4014e4cc2cbcd579c008
LUCIANO BERWANGER WILLE <52546153+LucianoBWille@users.noreply.github.com>
2019-10-12 17:26:52 -0300

    Create Exercício005.py

---
+++ Exercícios Curso em Vídeo/Exercício005.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+entrada = int(input("Digite um número:"))
+
+print("Antecessor: ", entrada-1)
+print("Sucessor: ", entrada+1)
