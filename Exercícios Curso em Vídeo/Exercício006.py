dab2f02cf0a63a6cc1f54d505b356051cf29f092
LUCIANO BERWANGER WILLE <52546153+LucianoBWille@users.noreply.github.com>
2019-10-12 17:26:32 -0300

    Create Exercício006.py

---
+++ Exercícios Curso em Vídeo/Exercício006.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+entrada = int(input("Digite um número:"))
+
+print("dobro: {}".format((entrada*2)))
+print("triplo: {}".format((entrada*3)))
+print("raiz: {}".format((entrada**(1/2))))
