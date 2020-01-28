4993269478efdf2403523fabd4a0c50e4f1c73fe
LUCIANO BERWANGER WILLE <52546153+LucianoBWille@users.noreply.github.com>
2020-01-28 15:31:50 -0300

    Update Exercício007.py

--- Exercícios Curso em Vídeo/Exercício007.py
+++ Exercícios Curso em Vídeo/Exercício007.py
@@ -5,3 +5,4 @@ n1 = float(input('Digite a primeira nota:'))
 n2 = float(input('Digite a segunda nota:'))
 
 print('Média do aluno:',(n1+n2)/2)
+print('Média do aluno aredondada: {:.1f}'.format((n1+n2)/2)) 
