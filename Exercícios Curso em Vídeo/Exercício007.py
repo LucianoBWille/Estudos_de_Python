8bbffa249ed53006bd42ac4a38d665926999032d
LUCIANO BERWANGER WILLE <52546153+LucianoBWille@users.noreply.github.com>
2020-01-28 15:24:56 -0300

    Update Exercício007.py

--- Exercícios Curso em Vídeo/Exercício007.py
+++ Exercícios Curso em Vídeo/Exercício007.py
@@ -1,1 +1,7 @@
-# 
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+n1 = float(input('Digite a primeira nota:'))
+
+n2 = float(input('Digite a segunda nota:'))
+
+print('Média do aluno:',(n1+n2)/2)
