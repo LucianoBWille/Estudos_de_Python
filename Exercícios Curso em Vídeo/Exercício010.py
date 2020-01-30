a42299c7f6cb1dfa05d97c72dbe77b8580c58426
LUCIANO BERWANGER WILLE <52546153+LucianoBWille@users.noreply.github.com>
2020-01-30 12:20:29 -0300

    Update Exercício010.py

--- Exercícios Curso em Vídeo/Exercício010.py
+++ Exercícios Curso em Vídeo/Exercício010.py
@@ -1,1 +1,9 @@
-# 
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+#
+# Considerando o dolar a R$3.27
+
+preço_do_dolar = 3.27
+valor_em_reais = float(input('Digite o quanto dinheiro tem: R$'))
+valor_em_dolares = valor_em_reais / preço_do_dolar
+
+print('Com R${:.2f} você pode comprar US${:.2f}'.format(valor_em_reais, valor_em_dolares))
