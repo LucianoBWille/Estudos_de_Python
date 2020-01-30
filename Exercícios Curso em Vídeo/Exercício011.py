9ea9a0364b322814af60a5e3b9e0819c70777ca1
LUCIANO BERWANGER WILLE <52546153+LucianoBWille@users.noreply.github.com>
2020-01-30 12:21:44 -0300

    Update Exercício011.py

--- Exercícios Curso em Vídeo/Exercício011.py
+++ Exercícios Curso em Vídeo/Exercício011.py
@@ -1,1 +1,9 @@
-# 
+# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+valor_original = float(input('Qual é o valor do produto? R$'))
+
+percentual_de_desconto = 5
+
+valor_com_desconto: float = valor_original * (1 - percentual_de_desconto/100)
+
+print('O produto que custava R${:.2f}, na promoção com desconto de {}% vai custar R${:.2f}'.format(valor_original, percentual_de_desconto, valor_com_desconto))
