f9cd3599185ad62bb33e33532f46caeb87d56fbe
LUCIANO BERWANGER WILLE <52546153+LucianoBWille@users.noreply.github.com>
2020-01-30 12:18:44 -0300

    Update Exercício009.py

--- Exercícios Curso em Vídeo/Exercício009.py
+++ Exercícios Curso em Vídeo/Exercício009.py
@@ -1,1 +1,8 @@
-# 
+# Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+valor = int(input('Digite um número para ver sua tabuada: '))
+
+print('-' * 18)
+for i in range(1,11):
+    print('| {} x {:2}  = {:3} |'.format(valor,i,valor*i))
+print('-' * 18)
