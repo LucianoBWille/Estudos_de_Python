1fcb5b67b18e8ee69c7d0184f89cf39bae2deafb
LUCIANO BERWANGER WILLE <52546153+LucianoBWille@users.noreply.github.com>
2020-01-30 12:18:15 -0300

    Update Exercício008.py

--- Exercícios Curso em Vídeo/Exercício008.py
+++ Exercícios Curso em Vídeo/Exercício008.py
@@ -1,1 +1,10 @@
-# 
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+medida = float(input('Digite uma distancia em metros: '))
+
+print('\n{} km'.format(medida/1000))
+print('{} hm'.format(medida/100))
+print('{} dam'.format(medida/10))
+print('{} dm'.format(medida*10))
+print('{} cm'.format(medida*100))
+print('{} mm'.format(medida*1000))
