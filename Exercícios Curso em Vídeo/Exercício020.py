38086847f7a92138132e934aa5084ffe5aa149bf
LUCIANO BERWANGER WILLE <52546153+LucianoBWille@users.noreply.github.com>
2020-02-01 11:21:22 -0300

    Update Exercício020.py

--- Exercícios Curso em Vídeo/Exercício020.py
+++ Exercícios Curso em Vídeo/Exercício020.py
@@ -1,1 +1,15 @@
-# 
+# O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que
+# leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+from random import shuffle
+alunos = []
+primeiro = input('Primeiro aluno: ')
+segundo = input('Segundo aluno: ')
+terceiro = input('Terceiro aluno: ')
+quarto = input('Quarto aluno: ')
+
+alunos = [primeiro, segundo, terceiro, quarto]
+
+shuffle(alunos) # misturar
+
+print('O aluno escolhido foi {}'.format(alunos))
