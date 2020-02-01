eb5d0552a7e9d440086451b707419b8036743fb7
LUCIANO BERWANGER WILLE <52546153+LucianoBWille@users.noreply.github.com>
2020-02-01 11:18:26 -0300

    Update Exercício019.py

--- Exercícios Curso em Vídeo/Exercício019.py
+++ Exercícios Curso em Vídeo/Exercício019.py
@@ -1,1 +1,13 @@
-# 
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o
+# nome dos alunos e escrevendo na tela o nome do escolhido.
+
+from random import choice
+alunos = []
+primeiro = input('Primeiro aluno: ')
+segundo = input('Segundo aluno: ')
+terceiro = input('Terceiro aluno: ')
+quarto = input('Quarto aluno: ')
+
+alunos = [primeiro, segundo, terceiro, quarto]
+
+print('O aluno escolhido foi {}'.format(choice(alunos)))
