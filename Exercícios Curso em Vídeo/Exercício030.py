d0a2307f14c8c88769ed7f4384f37984ec293082
Luciano Berwanger Wille <00lucianobwille00@gmail.com>
2020-02-17 13:22:05 -0300

    Exercício 30 resolvido

--- Exercícios Curso em Vídeo/Exercício030.py
+++ Exercícios Curso em Vídeo/Exercício030.py
@@ -1,1 +1,7 @@
-# 
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+numero = int(input('Me diga um número qualquer: '))
+if numero % 2 == 0:
+    print('O número {} é \033[1;34mPAR\033[m'.format(numero))       # \033[m é o codigo para estilizar o texto
+else:                                                               # o numero 1 se refere a negrito
+    print('O número {} é \033[1;34mÍMPAR\033[m'.format(numero))     # o número 34 é a cor azul para letra
--- Exercícios Curso em Vídeo/README.md
+++ Exercícios Curso em Vídeo/README.md
@@ -94,7 +94,7 @@ Exercício029:
   Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
 
 Exercício030:
-  
+  Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
 
 Exercício031:
 
