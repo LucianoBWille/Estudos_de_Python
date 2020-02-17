7f1aaf11e2f8739763a7f4f716dbca49fb3bdbdf
Luciano Berwanger Wille <00lucianobwille00@gmail.com>
2020-02-17 15:19:01 -0300

    Exercício 32 resolvido

--- Exercícios Curso em Vídeo/Exercício032.py
+++ Exercícios Curso em Vídeo/Exercício032.py
@@ -1,1 +1,13 @@
-# 
+# Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
+
+from datetime import datetime
+
+ano = int(input('Que ano quer analisar? Coleque 0 para analisar o ano atual: '))
+
+if ano == 0:
+    ano = datetime.now().year
+
+if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0:
+    print('O ano {} é \033[1;33mBISSEXTO\033[m'.format(ano))  # BISSEXTO em negrito e em amarelo
+else:
+    print('O ano {} \033[1;31mNÃO\033[m é \033[1;33mBISSEXTO\033[m'.format(ano))  # NÃO em negrito e em vermelho
--- Exercícios Curso em Vídeo/README.md
+++ Exercícios Curso em Vídeo/README.md
@@ -100,6 +100,7 @@ Exercício031:
   Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.
 
 Exercício032:
+  Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
 
 Exercício033:
 
