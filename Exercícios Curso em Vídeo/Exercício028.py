b7b50d70bf9290430f62f63f509a0cd10dce3aca
Luciano Berwanger Wille <00lucianobwille00@gmail.com>
2020-02-16 23:50:57 -0300

    Exercício 28 resolvido

--- Exercícios Curso em Vídeo/Exercício028.py
+++ Exercícios Curso em Vídeo/Exercício028.py
@@ -1,1 +1,17 @@
-# 
+# Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar
+# descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
+from random import randint
+#from time import sleep
+
+valor = randint(0, 5)
+print('Vou pensar em um número de 0 a 5. Tente adivinhar...')
+palpite = int(input('Em que número eu pensei? '))
+
+#print('PROCESSANDO...')
+#sleep(1)
+
+if palpite == valor:
+    print('PARABÉNS! Você conseguiu vencer!')
+else:
+    print('GANHEI! Eu pensei no número {} e não no {}!'.format(valor, palpite))
--- Exercícios Curso em Vídeo/README.md
+++ Exercícios Curso em Vídeo/README.md
@@ -88,6 +88,7 @@ Exercício027:
   Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 
 Exercício028:
+  Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
 Exercício029:
 
