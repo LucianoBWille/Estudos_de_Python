c92a7175e6b5ae80b9420e579f60817257cc2f75
Luciano Berwanger Wille <00lucianobwille00@gmail.com>
2020-02-20 21:28:48 -0300

    Exercício 36 resolvido

--- Exercícios Curso em Vídeo/Exercício036.py
+++ Exercícios Curso em Vídeo/Exercício036.py
@@ -1,1 +1,14 @@
-# 
+# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o
+# salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou
+# então o empréstimo será negado.
+
+casa = float(input('Valor da casa: R$ '))
+salario = float(input('Salário do comprador: R$ '))
+anos = int(input('Qunntos anos de financiamento? '))
+meses = anos * 12
+parcela = casa / meses
+print('Para pagar o empréstimo de R${:.2f} em {} anos a prestação será de R${:.2f}'.format(casa, anos, parcela))
+if parcela > salario * 0.3:
+    print('Empréstimo NEGADO!')
+else:
+    print('Empréstimo pode ser CONCEDIDO!')
--- Exercícios Curso em Vídeo/README.md
+++ Exercícios Curso em Vídeo/README.md
@@ -112,6 +112,7 @@ Exercício035:
   Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
 
 Exercício036:
+  Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
 
 Exercício037:
 
