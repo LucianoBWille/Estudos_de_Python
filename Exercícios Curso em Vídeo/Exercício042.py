a74aae35a336df0ced1e5c5d30031bdd9ca50bef
Luciano Berwanger Wille <00lucianobwille00@gmail.com>
2020-02-21 01:51:57 -0300

    Exercício 42 resolvido

--- Exercícios Curso em Vídeo/Exercício042.py
+++ Exercícios Curso em Vídeo/Exercício042.py
@@ -1,1 +1,23 @@
-# 
+# Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+# - EQUILÁTERO: todos os lados iguais
+# - ISÓSCELES: dois lados iguais, um diferente
+# - ESCALENO: todos os lados diferentes
+
+print('=' + '-=' * 12)
+print('Analizador de Triângulos')
+print('=' + '-=' * 12)
+
+a = float(input('Primeiro segmento: '))
+b = float(input('Segundo segmento: '))
+c = float(input('Terceiro segmento: '))
+
+if abs(b - c) < a < (b + c) and abs(a - c) < b < (a + c) and abs(a - b) < c < (a + b):
+    print('Os segmentos PODEM FORMAR um triângulo ', end='')
+    if a == b == c:
+        print('EQUILÁTERO')
+    elif a == b or a == c or b == c:
+        print('ISÓSCELES')
+    else:
+        print('ESCALENO')
+else:
+    print('Os segmentos NÃO PODEM FORMAR um triângulo!')
